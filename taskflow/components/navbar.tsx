9c50abe29441ca36cf6daa5b95d1975dcae11f9c
Saujanyatiwari <saujanya.tiwari03@gmail.com>
2026-05-02 01:28:55 +0530

    initial dashbaord setup

--- taskflow/components/navbar.tsx
+++ taskflow/components/navbar.tsx
@@ -1,14 +1,37 @@
 "use client";
 
-import { SignInButton, SignUpButton, useUser } from "@clerk/nextjs";
+import { SignInButton, SignUpButton, UserButton, useUser } from "@clerk/nextjs";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
+import { usePathname } from "next/dist/client/components/navigation";
 
 // import {TaskFlow} from "lucide-react";
 export default function Navbar() {
 
     const {isSignedIn , user} = useUser();
+    const pathname = usePathname();
+
+    // const isHomePage = pathname === "/";
+    const isDashboardPage = pathname ==="/dashboard";
+    const isBoardPage = pathname.startsWith("/board/"); // this checks the board page with its dynamic id
+
+    if(isDashboardPage){
+        return(
+        <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50"> 
+            <div className="container mx-auto px-4 py-3 sm:py-4 flex items-center justify-between">
+                <div className="flex items-center space-x-2"> 
+                    <div className="h-6 w-6 sm:h-8 sm:w-8 text-blue-600"/>
+                    <span className="text-xl sm:text-2xl font-bold text-gray-900">TaskFlow</span>
+                </div>
+
+                <div className="flex items-center space-x-2 sm:space-x-4">
+                    <UserButton />
+                 </div>
+            </div>
+        </header>
+        );
+    }
 
     return(
         <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50"> 
